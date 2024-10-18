7bcc033d0fc3e81af703551eb162ff317d46af2d
Gabriel <gabriel.bc04@hotmail.com>
2024-10-18 22:17:59 +0200

    Spiel v5.0
    - GridPane in einem BorderPane gepackt.
    
    - Die Quadraten wurden von Buttons ersetzt.
    
    - Beim Klicken eines Buttons wird das geklickte Button mit dem leeren Button wechseln, falls dieser neben ihn steht.

--- puzzle/src/main/java/org/gkl/Spiel.java
+++ puzzle/src/main/java/org/gkl/Spiel.java
@@ -1,10 +1,16 @@
 package org.gkl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -12,81 +18,82 @@ import javafx.stage.Stage;
 
 public class Spiel extends Application {
 
-    private static final int gridGroesse = 5;
+    private static final int gridGroesse = 3;
     private static final int quadratGroesse = 100;
-
-    // verschachtelte Array für X und Y werte
-    private Rectangle[][] quadraten = new Rectangle[gridGroesse][gridGroesse];
-
-    private int reiheAkt = 0;
-    private int spalteAkt = 0;
+    Button button;
+    List<Button> buttons = new ArrayList<>();
+    int index;
+    private Button buttonLeer;
 
     @Override
     public void start(Stage primaryStage) {
-        // gridpane erstellen
-        GridPane gridPane = new GridPane();
 
-        Scene scene = new Scene(gridPane, gridGroesse * quadratGroesse, gridGroesse * quadratGroesse);
+        // Erstellung von Elemente
+        Label titelSpiel = new Label("GKL Puzzle");
+        titelSpiel.setTextFill(Color.RED);
+
+        // Erstellung von Panes
+        GridPane gridPane = new GridPane();
+        BorderPane borderPane = new BorderPane();
+
+        // Bereiche der borderPane organisieren
+        borderPane.setTop(titelSpiel);
+        borderPane.setAlignment(titelSpiel, Pos.CENTER);
+        // center
+        borderPane.setCenter(gridPane);
+        gridPane.setAlignment(Pos.CENTER);
+        // bottom
+        BorderPane.setAlignment(gridPane, Pos.CENTER);
+        borderPane.setBottom(new Label("Tastenkombinationen"));
+
+        // Erstellung der Buttons
+        for (int i = 0; i < gridGroesse * gridGroesse; i++) {
+            Button button = new Button(Integer.toString(i + 1));
+            // Button selbst soll so groß sein
+            button.setPrefWidth(quadratGroesse);
+            button.setPrefHeight(quadratGroesse);
+            // Zahl des Buttons soll so groß sein
+            button.setStyle("-fx-font-size: 20px;");
+
+            button.setOnAction(e -> buttonBewegen(button));// Eventlistener beim Klicken
+            buttons.add(button);
+        }
+
+        // Buttons ins gridPane einfügen
+        for (int reiheAkt = 0; reiheAkt < gridGroesse; reiheAkt++) {
+            for (int spalteAkt = 0; spalteAkt < gridGroesse; spalteAkt++) {
+                index = reiheAkt * gridGroesse + spalteAkt;
+                gridPane.add(buttons.get(index), reiheAkt, spalteAkt);
+            }
+        }
+
+        buttonLeer = buttons.get(0);
+        buttonLeer.setText("");
+
+        gridPane.requestFocus();
+        // Beim druclen der Pfeile, werden die Buttons nicht selektiert
+        gridPane.setFocusTraversable(true);
+        borderPane.setFocusTraversable(true);
+
+        // Das Fenster muss 640x480 Groß sein
+        Scene scene = new Scene(borderPane, 640, 480);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Schiebepuzzle");
         primaryStage.show();
-
-        // Würfeln initialisieren
-        for (int reihe = 0; reihe < gridGroesse; reihe++) {
-            for (int spalte = 0; spalte < gridGroesse; spalte++) {
-                Rectangle quadrat = new Rectangle(quadratGroesse, quadratGroesse);
-                // if reihe == reiheAkt und spalte == spalteAkt, dann Farbe rot, sonst blau
-                quadrat.setFill(reihe == reiheAkt && spalte == spalteAkt ? Color.RED : Color.BLUE);
-                quadrat.setStroke(Color.BLACK);
-                quadraten[reihe][spalte] = quadrat;
-                gridPane.add(quadrat, reihe, spalte);
-            }
-        }
-        // ohne das, Funktionieren die Tastatuureingaben nicht
-        gridPane.requestFocus();
-
-        // Input von Tastatur
-        scene.setOnKeyPressed(event -> {
-
-            switch (event.getCode()) {
-                case UP:
-                    quadratBewegen(0, -1);
-                    break;
-                case DOWN:
-                    quadratBewegen(0, 1);
-                    break;
-                case LEFT:
-                    quadratBewegen(-1, 0);
-                    break;
-                case RIGHT:
-                    quadratBewegen(1, 0);
-                    break;
-                case ESCAPE:
-                    Platform.exit();
-                    break;
-            }
-
-        });
     }
 
-    public void quadratBewegen(int rowChange, int colChange) {
-        // holt Farbe der aktuelle Stelle
-        Color farbeAktuell = (Color) quadraten[reiheAkt][spalteAkt].getFill();
-        // position aktualisieren
-        reiheAkt += rowChange;
-        spalteAkt += colChange;
-
-        // nächste Stelle darf nicht außerhalb des gridPanes sein
-        if (!(reiheAkt < 0 | spalteAkt < 0 | reiheAkt > gridGroesse - 1 | spalteAkt > gridGroesse - 1)) {
-            // holt die Farbe der nächste Stelle
-            Color farbeNext = (Color) quadraten[reiheAkt][spalteAkt].getFill();
-            // Farbe wechseln
-            quadraten[reiheAkt][spalteAkt].setFill(farbeAktuell);
-            quadraten[reiheAkt - rowChange][spalteAkt - colChange].setFill(farbeNext);
-        } else {
-            // position zurücksetzen
-            reiheAkt -= rowChange;
-            spalteAkt -= colChange;
+    private void buttonBewegen(Button button) {
+        if (buttonNeben(button, buttonLeer)) {
+            String buttonZahl = button.getText();
+            button.setText(buttonLeer.getText());
+            buttonLeer.setText(buttonZahl);
+            buttonLeer = button;
         }
+    }
+
+    private boolean buttonNeben(Button buttonZahl, Button buttonLeer) {
+        int indexZahl = buttonZahl.getParent().getChildrenUnmodifiable().indexOf(buttonZahl);
+        int indexLeer = buttonLeer.getParent().getChildrenUnmodifiable().indexOf(buttonLeer);
+        return Math.abs(indexZahl - indexLeer) == 1 || Math.abs(indexZahl - indexLeer) == gridGroesse;
     }
 }
