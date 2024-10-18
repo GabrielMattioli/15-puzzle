6b1b0e96f773b8bc435aed5666f238366963174d
Gabriel <gabriel.bc04@hotmail.com>
2024-10-18 21:23:47 +0200

    Spiel v2.0
    Zahlen von 1 bis 15 werden nebeneinander angeordnet angezeigt.

--- puzzle/src/main/java/org/gkl/Spiel.java
+++ puzzle/src/main/java/org/gkl/Spiel.java
@@ -3,34 +3,37 @@ package org.gkl;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.util.Random;
-
 public class Spiel extends Application {
+    private static final int viereckGroesse = 50;
+    private static final int reiheAnz = 4;
+    private static final int spaltenAnz = 4;
+
     @Override
     public void start(Stage primaryStage) {
-        int reiheAnz = 10;
-        int spalteAnz = 10;
-        int gridHoehe = 300;
-        int gridBreite = 300;
-
-        GridPane grid = new GridPane();
-        Random rand = new Random();
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(0);
+        gridPane.setVgap(0);
 
         for (int reihe = 0; reihe < reiheAnz; reihe++) {
-            for (int spalte = 0; spalte < spalteAnz; spalte++) {
-                Rectangle rec = new Rectangle();
-                rec.setWidth(23);
-                rec.setHeight(23);
-                GridPane.setRowIndex(rec, reihe);
-                GridPane.setColumnIndex(rec, spalte);
-                grid.getChildren().add(rec);
+            for (int spalte = 0; spalte < spaltenAnz; spalte++) {
+                Rectangle viereck = new Rectangle(viereckGroesse, viereckGroesse, Color.LIGHTGRAY);
+                Text nummer = new Text(String.valueOf(reihe * spaltenAnz + spalte + 1));
+                StackPane stackPane = new StackPane(viereck, nummer);
+                gridPane.add(stackPane, spalte, reihe);
             }
         }
 
-        Scene scene = new Scene(grid, gridHoehe, gridBreite);
+        Rectangle viereckLeer = new Rectangle(viereckGroesse, viereckGroesse, Color.WHITE);
+        StackPane paneLeer = new StackPane(viereckLeer);
+        gridPane.add(paneLeer, spaltenAnz - 1, reiheAnz - 1);
+
+        Scene scene = new Scene(gridPane, spaltenAnz * viereckGroesse, reiheAnz * viereckGroesse);
         primaryStage.setTitle("Schiebepuzzle");
         primaryStage.setScene(scene);
         primaryStage.show();
